d17e350c596308fd7e39de8ebecc5f028273c88b
fizzayscode <hbumar100@gmail.com>
2022-10-13 12:01:17 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'using printf to print a string to console'
+ *
+ * Return: always return (0)
+ */
+ int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
