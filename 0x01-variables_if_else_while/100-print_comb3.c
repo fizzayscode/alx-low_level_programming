3d35745aa0407c145913631291440445560a18c4
fizzayscode <hbumar100@gmail.com>
2022-10-14 14:16:55 -0700

    1-100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Description: print all combination of single
+ * decimals
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y, current, next;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (x == y)
+				continue;
+			current = x * 10 + y;
+			next = y * 10 + x;
+			if (current < next)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				if (x <= 8 && y <= 9)
+				{
+					if (!(x == 8 && y == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
