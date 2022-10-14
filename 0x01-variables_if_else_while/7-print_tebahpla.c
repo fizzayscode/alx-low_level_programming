2dd6a1b43e1c90ec2973020d63e1c81685295112
fizzayscode <hbumar100@gmail.com>
2022-10-14 12:57:12 -0700

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main -Entry point
+*
+* Description: print from z -a
+* Return: Aways 0 (Success)
+*/
+int main(void)
+{
+	char a = 'z';
+
+	while (a != 96)
+	{
+		putchar(a);
+		a--;
+	}
+	return (0);
+}
