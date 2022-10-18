2a02f5a66445a91e1f04456bce790656a9bd877d
fizzayscode <hbumar100@gmail.com>
2022-10-18 12:13:52 -0700

    is upper

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - check integer intered is lowercase of not
+ *
+ * @c: is the integer we will check
+ *
+ * Return: return 1 if alphabets and 1 is not
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c >= 122))
+		return (1);
+	return (0);
+}
