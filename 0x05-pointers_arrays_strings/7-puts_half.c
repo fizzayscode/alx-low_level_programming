15d269ee975c26c98d956f28a0b5f77071867cc4
fizzayscode <hbumar100@gmail.com>
2022-10-25 13:41:07 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - print half
+ *
+ * @str: string
+ *
+ * Return: noting;
+ */
+void puts_half(char *str)
+{
+	int i, l, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	l = i;
+	if (len % 2 == 0)
+	l = len / 2;
+	else
+	l = (len - 1) / 2;
+	for (j = l; j <= i - 1; j++)
+	{
+		_putchar(str[j]);
+	}
+	putchar('\n');
+}
+
