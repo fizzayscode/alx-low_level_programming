72b0aa239aced0795838b80401bb15fd4c454b6f
fizzayscode <hbumar100@gmail.com>
2022-10-26 12:03:42 -0700

    strcopy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ *
+ * @dest: destination
+ *
+ * @src: source string
+ *
+ * @n: number of element to copy
+ *
+ * Return: return string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+		if (j < n)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
