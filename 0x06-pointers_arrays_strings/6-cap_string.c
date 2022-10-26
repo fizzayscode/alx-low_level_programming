54c8c8da2d0adc1dcc42891f4e9e8f069e3ded58
fizzayscode <hbumar100@gmail.com>
2022-10-26 14:14:03 -0700

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * cap_string - capitalise all words
+ *
+ * @a: paragraph to capicalise
+ *
+ * Return: capitalised string
+ */
+char *cap_string(char *a)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+	if (a[i] == ' ' || a[i] == '\n' || a[i] == '\t' || a[i] == '.')
+	{
+		++i;
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = a[i] - 32;
+	}
+	}
+
+	return (a);
+}
