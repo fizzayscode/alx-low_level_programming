edf0fdd735c35c96067cb3655a317d5932f3f2e1
fizzayscode <hbumar100@gmail.com>
2022-10-31 10:05:27 -0700

    memcopy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (n > 0)
 	{
 		dest[i] = src[i];
+		i++;
+		n--;
 	}
 	return (dest);
 }
